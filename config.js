47e14a20b868b0e50fe3651721c1f57865149da4
hlengiwe <hlengiwe.hn5@gmail.com>
2021-12-21 00:43:41 +0200

    new files added

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 
 
-let rootPath ="https://mysite.itvarsity.org/api/ContactBook/";   
+let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
 let apiKey = checkApiKey();
 
-function checkApiKey() {    
-    if (!localStorage.getItem("apiKey")) {
+function checkApiKey(){
+    if (!localStorage.getItem("apiKey")){
         window.open("enter-api-key.html", "_self");
-    } 
-    return localStorage.getItem("apiKey");
+    }
+    return localStorage.getItem("apiKey")
 }
